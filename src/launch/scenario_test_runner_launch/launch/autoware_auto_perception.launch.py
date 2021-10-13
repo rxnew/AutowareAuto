72a05ebe2d7599d0c648fe38bd7a44f82bb327d1
rxnew <rxnew.axdseuan@gmail.com>
2021-10-14 02:11:19 +0900

    Separate launch for scenario simulation

---
+++ src/launch/scenario_test_runner_launch/launch/autoware_auto_perception.launch.py
@@ -0,0 +1,40 @@
+# Copyright 2021, The Autoware Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Developed by Robotec.ai.
+
+"""Launch required subsystems for TierIV scenario_test_runner."""
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, OpaqueFunction, LogInfo
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def launch_setup(context, *args, **kwargs):
+    autoware_launch_prefix = get_package_share_directory('autoware_auto_launch')
+
+    perception = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([autoware_launch_prefix,
+                                       '/launch/autoware_auto_perception.launch.py']),
+        launch_arguments={}.items()
+    )
+
+    return [
+        perception
+    ]
+
+
+def generate_launch_description():
+    return LaunchDescription([OpaqueFunction(function=launch_setup)])
